4018d30ca23f8b5af38278f692ad884f124f8183
Mohd Fahad <88942913+MohdFahadIITK@users.noreply.github.com>
2026-02-23 22:43:51 +0530

    Rename file to C1 - Lost Civilization (Easy Version).cpp

--- 𝐂𝐨𝐝𝐞𝐟𝐨𝐫𝐜𝐞𝐬 𝐑𝐨𝐮𝐧𝐝 1082 (𝐃𝐢𝐯. 2)/C1 - Lost Civilization (Easy Version).cpp
+++ 𝐂𝐨𝐝𝐞𝐟𝐨𝐫𝐜𝐞𝐬 𝐑𝐨𝐮𝐧𝐝 1082 (𝐃𝐢𝐯. 2)/C1 - Lost Civilization (Easy Version).cpp
@@ -92,58 +92,54 @@ const int N = 3e5, M = N;
 vi g[N];
 
 //fahad.cpp
-vector<ll> oddmax(ll n){
-    if(n==1){
-        return {1,1};
-    }
-    ll first_av=1;
-    ll second_av=2;
-    vector<ll> ans;
-    ans.pb(1);
-    ans.pb(2);
-    ll num1=3;
-    ll num2=1;
-    for(int i=2;i<2*n-2;i++){
-        if(i%2==0){
-            ans.pb(num1);
-            num1++;
-        }
-        else{
-            ans.pb(num2);
-            num2++;
-        }
-    }
-    ans.pb(n-1);
-    ans.pb(n);
-    return ans;
-}
+
 void karke_dekhte_hain(){
     ll i=0,j=0,k=0,q=0,n=0,m=0,count=0,count1=0;
     vector<vector<ll>> v;
     vector<ll> a,b;
     string s,s1,s2;
-    cin>>n>>k;
-    if(k<n|k>=2*n){
-        cout<<"NO\n";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>j;
+        a.pb(j);
     }
-    else{
-        if(k==2*n-1){
-            cout<<"YES\n";
-            vector<ll> ans=oddmax(n);
-            prnt(ans);
+    vector<pair<ll,ll>> vip;
+    count=1;
+    for(int i=1;i<n;i++){
+        if(a[i]!=a[i-1]){
+            vip.pb({a[i-1],count});
+            count=1;
         }
         else{
-            ll freepair=2*n-k-1;
-            ll od2=n-freepair;
-            cout<<"YES\n";
-            vector<ll> ans=oddmax(od2);
-            for(int i=od2+1;i<=n;i++){
-                ans.pb(i);
-                ans.pb(i);
-            }
-            prnt(ans);
+            count++;
         }
     }
+    vip.pb({a[n-1],count});
+    map<ll,ll> mp;
+    mp[vip[0].F]=1;
+    ll ans=n;
+    set<ll> last_set;
+    last_set.insert(vip[0].F);
+    for(int i=1;i<vip.size();i++){
+        ll curr_branch=vip[i].F;
+        if(last_set.find(curr_branch-1) != last_set.end()){
+            ans-=vip[i].S;
+            vector<ll> rem;
+            for(auto it: last_set){
+                ll value=(it);
+                if(value>curr_branch) rem.pb(it);
+            }
+            for(auto it: rem){
+                last_set.erase(it);
+            }
+            last_set.insert(vip[i].F);
+        }
+        else{
+            last_set.clear();
+            last_set.insert(vip[i].F);
+        }
+    }
+    cout<<ans<<endl;
 }
 
 int main() {
